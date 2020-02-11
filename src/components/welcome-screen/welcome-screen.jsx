167a1b61a762d5f02e030aaf99616a528f71de67
ilataleksey <ilataleksey@gmail.com>
2020-02-11 20:38:52 +0300

    Удаляет комментарий, отключающий проверку ESlint

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const WelcomeScreen = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
   return (
     <section className="welcome">
