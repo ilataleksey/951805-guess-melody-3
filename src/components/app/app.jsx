e9264a731c22d8207ccf90cef6f93bb3ef8d144b
ilataleksey <ilataleksey@gmail.com>
2020-02-16 19:12:13 +0300

    Перемещает handler во внутрь компонента

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
-const welcomeButtonHandler = () => {};
-
 const App = (props) => {
   const {errorsCount} = props;
+  const welcomeButtonHandler = () => {};
   return (
     <WelcomeScreen
       errorsCount={errorsCount}
