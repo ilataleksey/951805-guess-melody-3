d7a50ace274df9c45651d2559121c101e1639034
ilataleksey <ilataleksey@gmail.com>
2020-02-23 21:59:54 +0300

    Добавляет компонент AudioPlayer и подключает к игровым экранам

---
+++ src/components/audio-player/audio-player.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AudioPlayer from './audio-player.jsx';
+import song from './audio-player.test.mocks.jsx';
+
+it(`AudioPlayer is rendered correctly`, () => {
+
+  const tree = renderer.create(<AudioPlayer
+    isPlaying={false}
+    src={song.src}
+  />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/audio-player/audio-player.test.mocks.jsx
@@ -0,0 +1,3 @@
+export default {
+  src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+};
