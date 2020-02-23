b3e35092a6a068611a235ff59c86da5c4bd73c7d
ilataleksey <ilataleksey@gmail.com>
2020-02-23 21:05:24 +0300

    Подключает в e2e-test ArtistQuestionScreen компонента моки из внешнего файла

--- src/components/artist-question-screen/artist-question-screen.e2e.test.jsx
+++ src/components/artist-question-screen/artist-question-screen.e2e.test.jsx
@@ -3,43 +3,19 @@ import {configure, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import ArtistQuestionScreen from "./artist-question-screen.jsx";
+import question from "./artist-question-screen.test.mocks.js";
 
 configure({adapter: new Adapter()});
-
-const mock = {
-  question: {
-    type: `artist`,
-    song: {
-      artist: ``,
-      src: ``
-    },
-    answers: [
-      {
-        artist: `one`,
-        picture: `pic-one`,
-      },
-      {
-        artist: `two`,
-        picture: `pic-two`,
-      },
-      {
-        artist: `three`,
-        picture: `pic-three`,
-      },
-    ],
-  }
-};
 
 const mockEvent = {
   preventDefault() {}
 };
 
 it(`Click on user answer should pass to the callback data-object from which this answer was created`, () => {
-  const {question} = mock;
   const onAnswer = jest.fn();
   const userAnswer = {
-    artist: `one`,
-    picture: `pic-one`,
+    artist: `John Snow`,
+    picture: `https://api.adorable.io/avatars/128/0`,
   };
 
   const screen = shallow(<ArtistQuestionScreen
