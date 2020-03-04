e248093f95dfdcca80d23d84f7673f150fc177c4
ilataleksey <ilataleksey@gmail.com>
2020-03-04 22:04:00 +0300

    Создает Action Creator, убирает state из App, подключает App к Redux

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,15 +1,56 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import App from "./app.jsx";
-import questions from "./app.test.mocks.js";
+import {App} from "./app.jsx";
+import {questions} from "../../mocks/mocks-test.js";
 
-it(`Render App`, () => {
-  const tree = renderer
-    .create(<App
-      errorsCount={3}
-      questions={questions}
-    />)
-    .toJSON();
+describe(`Render App`, () => {
+  it(`Render WelcomeScreen`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={-1}
+      />)
+      .toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Render GenreQuestionScreen`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={0}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      })
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Render ArtistQuestionScreen`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={1}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      })
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/mocks/mocks-test.js
+++ src/mocks/mocks-test.js
@@ -1,4 +1,4 @@
-export default [
+export const questions = [
   {
     type: `genre`,
     genre: `rock`,
